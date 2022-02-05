a02b35f287e6f4932f8b28d0e4083d5fe4f22e12
danvu <danvu@ya.ru>
2022-02-06 00:10:32 +0300

    added HardTest

--- src/test/java/HardTest.java
+++ src/test/java/HardTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class HardTest {
+
+    @Test
+    void AssertNotTrue() {
+        Assertions.assertTrue(2 > 1);
+    }
 }
