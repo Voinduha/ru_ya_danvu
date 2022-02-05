3d0934e13a04bb7be896ba050f473c0eb7b1b22a
Daniel Vulikh <62853995+Voinduha@users.noreply.github.com>
2022-02-06 00:21:32 +0300

    Added 3 Tests (#1)
    * add SimpleTest
    
    * JustSimpleTest.java
    
    * NotSimpleTest.java

---
+++ src/test/java/ru/ya/danvu/SimpleTest.java
@@ -0,0 +1,12 @@
+package ru.ya.danvu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void assertTrueTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
